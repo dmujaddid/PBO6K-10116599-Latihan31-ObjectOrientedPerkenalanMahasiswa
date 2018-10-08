3a2214c8becc4c9b193f643862105d6c91a50733
Roby <robyeka@email.unikom.ac.id>
2018-10-09 04:30:27 +0700

    first commit

--- src/pbo6k/pkg10116599/latihan31/objectorientedperkenalanmahasiswa/PBO6K10116599Latihan31ObjectOrientedPerkenalanMahasiswa.java
+++ src/pbo6k/pkg10116599/latihan31/objectorientedperkenalanmahasiswa/PBO6K10116599Latihan31ObjectOrientedPerkenalanMahasiswa.java
@@ -16,6 +16,18 @@ public class PBO6K10116599Latihan31ObjectOrientedPerkenalanMahasiswa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        mahasiswa mhs1 = new mahasiswa();
+        mhs1.perkenalanDiri("10110269", "Rizki Adam Kurniawan");
+        
+        mahasiswa mhs2 = new mahasiswa();
+        mhs2.perkenalanDiri("10110270", "Indra Tiola");
+        
+        mahasiswa mhs3 = new mahasiswa();
+        mhs3.perkenalanDiri("10110271", "Robi Tanzil Ganefi");
+        
+        mahasiswa mhs4 = new mahasiswa();
+        mhs4.perkenalanDiri("10110269", "Muhammad Nur Awaludin");
     }
     
 }
---
+++ src/pbo6k/pkg10116599/latihan31/objectorientedperkenalanmahasiswa/mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan31.objectorientedperkenalanmahasiswa;
+
+/**
+ *
+ * @author eka
+ */
+public class mahasiswa {
+    String mahasiswa;
+    String nim;
+    
+    public void perkenalanDiri(String parNim, String parNama) {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is " +parNim);
+        System.out.println("My Name is " +parNama);
+        System.out.println("");
+        
+    }   
+    
+}
